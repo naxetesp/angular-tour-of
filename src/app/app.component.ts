1165e95e3e93686e0215b8a48390f2d4b6afcf21
Ignacio Martin Palacios <ignacio.martin.palacios@everis.com>
2019-02-26 11:14:39 +0100

    prueba commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of';
+  title = 'Tour of Heroes';
 }
